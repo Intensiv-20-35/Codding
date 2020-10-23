7dc4e2e2c02ba58a75357015e45fe928669d96b2
AbbaVaR <73334837+AbbaVaR@users.noreply.github.com>
2020-10-23 18:35:18 +0500

    Add приложение

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int kab = int.Parse(textBox2.Text);
+            int korp = int.Parse(textBox2.Text);
+
+            if (kab < 200)
+            {
+                if (korp == 1)
+                {
+                    Form form2 = new Form2();
+                    form2.ShowDialog();
+
+                }
+
+                if (korp == 2)
+                {
+
+                }
+
+                if (korp == 3)
+                {
+
+                }
+
+                if (korp == 4)
+                {
+
+                }
+            }
+
+            else if (kab < 300)
+            {
+                if (korp == 1)
+                {
+
+                }
+
+                if (korp == 2)
+                {
+
+                }
+
+                if (korp == 3)
+                {
+
+                }
+
+                if (korp == 4)
+                {
+
+                }
+            }
+            else if (kab < 400)
+            {
+                if (korp == 1)
+                {
+
+                }
+
+                if (korp == 2)
+                {
+
+                }
+
+                if (korp == 3)
+                {
+
+                }
+
+                if (korp == 4)
+                {
+
+                }
+            }
+
+            else if (kab < 500)
+            {
+                if (korp == 1)
+                {
+
+                }
+
+                if (korp == 2)
+                {
+
+                }
+
+                if (korp == 3)
+                {
+
+                }
+
+                if (korp == 4)
+                {
+
+                }
+            }
+
+            else if (kab < 600)
+            {
+                if (korp == 1)
+                {
+
+                }
+
+                if (korp == 2)
+                {
+
+                }
+
+                if (korp == 3)
+                {
+
+                }
+
+                if (korp == 4)
+                {
+
+                }
+            }
+
+        }
+    }
+}
