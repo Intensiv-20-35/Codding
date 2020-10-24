643b42f4860463ce1832507905d1936df8199d85
Аббазов Валерьян <asr77734@gmail.com>
2020-10-24 13:24:40 +0500

    Изм начальная страница

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -30,7 +30,7 @@ namespace WindowsFormsApp1
 
             else if (kab < 300)
             {
-                    Form form3 = new Form3();
+                    Form form3 = new Form3(kab, korp);
                     form3.ShowDialog();
             }
             else if (kab < 400)
